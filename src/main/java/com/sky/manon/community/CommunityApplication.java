487ed61ca5605a2feb2157c385cc88c83acb09e4
linfei <huyibanbanbobo@163.com>
2019-09-28 10:30:04 +0800

    first commit

---
+++ src/main/java/com/sky/manon/community/CommunityApplication.java
@@ -0,0 +1,13 @@
+package com.sky.manon.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
